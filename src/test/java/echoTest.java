0f8dea9e8e66d0f5ffce41068b2a0e2b988c79e4
Anastasiya-Vavilova <ntttru@bk.ru>
2024-12-03 17:00:11 +0300

    finalization of tes

--- src/test/java/echoTest.java
+++ src/test/java/echoTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class echoTest {
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class echoTest {
+
+    @Test
+    public void test() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+
+    }
 }
